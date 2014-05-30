21895e54ec576ab99967f07cd431bdb6dfa0e5f1
Jason Li <jason.li@kitware.com>
2014-05-30 16:58:51 -0400

    use itemrevision_id field instead

--- controllers/components/NotificationComponent.php
+++ controllers/components/NotificationComponent.php
@@ -82,7 +82,7 @@ class Journal_NotificationComponent extends AppComponent
     $handle = $resourceDao->getHandle();
     $authors = $resourceDao->getAuthors();
     $itemId = $resourceDao->getItemId();
-    $revisionId = $resourceDao->getRevision()->getItemRevision_id();
+    $revisionId = $resourceDao->getRevision()->itemrevision_id;
     $authList = '';
     foreach ($authors as $author)
       {
