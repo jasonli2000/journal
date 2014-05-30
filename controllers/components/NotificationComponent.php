f373dcfd53e050111cb01d06021052fcb14a858b
Jason Li <jason.li@kitware.com>
2014-05-30 16:54:58 -0400

    use getRevision_id instead

--- controllers/components/NotificationComponent.php
+++ controllers/components/NotificationComponent.php
@@ -82,7 +82,7 @@ class Journal_NotificationComponent extends AppComponent
     $handle = $resourceDao->getHandle();
     $authors = $resourceDao->getAuthors();
     $itemId = $resourceDao->getItemId();
-    $revisionId = $resourceDao->getRevision()->getLastRevision();
+    $revisionId = $resourceDao->getRevision()->getRevision_id();
     $authList = '';
     foreach ($authors as $author)
       {
